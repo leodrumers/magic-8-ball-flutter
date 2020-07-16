c07f4a6bcb1a55324c50e6fb5f68ebc581e317bb
leodrumers <leodrumers@gmail.com>
2020-07-16 09:17:29 -0500

    Changing the state of the magic ball when press

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,39 @@
+import 'dart:math';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.blue,
+          appBar: AppBar(
+            title: Text('Magic ball'),
+            backgroundColor: Colors.blueAccent,
+          ),
+          body: Ball(),
+        ),
       ),
     );
+
+class Ball extends StatefulWidget {
+  @override
+  _BallState createState() => _BallState();
+}
+
+class _BallState extends State<Ball> {
+  int ballNumber = 1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: FlatButton(
+        child: Image.asset('images/ball$ballNumber.png'),
+        onPressed: () {
+          setState(() {
+            ballNumber = Random().nextInt(4) + 1;
+          });
+        },
+      ),
+    );
+  }
+}
